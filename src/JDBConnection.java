7288e662ec21a087e77116ff2f6e4e910bcc2da3
Rica <desiree2009@att.net>
2022-12-11 20:22:53 -0500

    Error fix, connection cleanup
    Created seperate open connector method to avoid 20 million connections; Axed if/else for case statements for cleaner code.

--- src/JDBConnection.java
+++ src/JDBConnection.java
@@ -1,11 +1,8 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 import java.util.Scanner;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 public class JDBConnection {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         // AllTables();
         // ReadColumn();
 
@@ -18,53 +15,71 @@ public class JDBConnection {
 
             System.out.println("Choose a function: 1.Display all tables; 2. ReadColumn; 3.Exit");
             int userInput = scanner.nextInt();
-            if (userInput == 1) {
-                RunApp();
+            switch(userInput) {
+                case 1:
+                    RunApp();
+                    break;
+                case 2:
+                    ReadColumn();
+                    break;
+                default:
+                    break;
+
+            }
+
             }
             // if statement here
-            System.out.println("Your input was.." + userInput);// remove this line after putting if statement
+           // System.out.println("Your input was.." + userInput);// remove this line after putting if statement
 
         }
 
-    }
+
 
     public static void RunApp() {
         AllTables(false);
     }
 
+
+
     public static void ReadColumn() {
+
 
         try {
 
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/airportdb", "root",
-                    "taco");
             // Line 20 is a custom based on your local database. Remove "airportdb" and
             // replace with the db that your using. Likewise, use the username and password
             // you used when setting up your database connection.
 
             // System.out.println("Connection Sucessful!.......");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/airportdb", "root",
+                    "taco");
+
+            System.out.println("Connection Sucessful!.......");
+
 
             Statement statement = connection.createStatement();
 
             ResultSet resultSet = statement.executeQuery("select * from airport");
 
             while (resultSet.next()) {
-                System.out.println(resultSet.getString("Country"));
+                System.out.println(resultSet.getString("ApCountry"));
+                System.out.println(resultSet.getString("ApCity"));
             }
-
+            statement.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 
     public static void AllTables(boolean run) {
         try {
             run = false;
+
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/airportdb", "root",
                     "taco");
 
             System.out.println("Connection Sucessful!.......");
-
             Statement statement = connection.createStatement();
 
             ResultSet resultSet = statement.executeQuery("Show tables");
